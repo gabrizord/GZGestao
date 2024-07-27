94cf22e008cf1615210d72a7f1db090e9191c06e
Gabriel <lolqueloko@gmail.com>
2024-07-27 12:46:21 -0300

    Initial commit

---
+++ src/main/java/br/com/gabrizord/gzgestao/GzGestaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrizord.gzgestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzGestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GzGestaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrizord/gzgestao/GzGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrizord.gzgestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
