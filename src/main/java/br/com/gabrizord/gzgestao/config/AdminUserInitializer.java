0eec493463b3b0bc88be6fdde00eaf79d2bd2c47
Gabriel <lolqueloko@gmail.com>
2024-07-28 03:32:31 -0300

    Remove unused imports and add CSS and HTML fragments
    - Removed unused imports `List` and `Set` from `AdminUserInitializer.java`.
    - Added new CSS files for `footer`, `header`, and `navbar`.
    - Created new HTML fragments for `footer`, `header`, and `navbar`.
    - Updated `home.html` to use the newly created HTML fragments.

--- src/main/java/br/com/gabrizord/gzgestao/config/AdminUserInitializer.java
+++ src/main/java/br/com/gabrizord/gzgestao/config/AdminUserInitializer.java
@@ -10,9 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Component
 public class AdminUserInitializer implements CommandLineRunner {
