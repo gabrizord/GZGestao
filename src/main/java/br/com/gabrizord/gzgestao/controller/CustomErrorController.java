d1862961f7dcf829adec56aaf36f59abc7a7990f
Gabriel <lolqueloko@gmail.com>
2024-08-17 16:32:43 -0300

    refactor(controller): replace @RequestMapping with @GetMapping in CustomErrorController
    Updated the CustomErrorController to use @GetMapping instead of @RequestMapping for the /error endpoint. This change ensures that the method is explicitly mapped to GET requests, improving code clarity and aligning with RESTful practices.

--- src/main/java/br/com/gabrizord/gzgestao/controller/CustomErrorController.java
+++ src/main/java/br/com/gabrizord/gzgestao/controller/CustomErrorController.java
@@ -5,12 +5,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    @RequestMapping("/error")
+    @GetMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if (status != null) {
