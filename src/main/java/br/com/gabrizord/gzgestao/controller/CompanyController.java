fea76de9684395297c5c7a1e3fe13795d3c55a05
Gabriel <lolqueloko@gmail.com>
2024-08-31 22:18:03 -0300

    feat(company): add pagination and sorting to company listing
    Implemented pagination and sorting functionality in the `CompanyController` to enhance the company listing page. The controller now accepts pagination parameters (`page`, `size`) and sorting parameters (`sortField`, `sortDirection`). The changes include updating the view model to pass these parameters and adjust the view accordingly.
    
    Updated the corresponding Thymeleaf template (`company.html`) to support sortable columns and formatted display for CNPJ and phone numbers. Also, added pagination controls to navigate through the paginated company list.

--- src/main/java/br/com/gabrizord/gzgestao/controller/CompanyController.java
+++ src/main/java/br/com/gabrizord/gzgestao/controller/CompanyController.java
@@ -2,11 +2,11 @@ package br.com.gabrizord.gzgestao.controller;
 
 import br.com.gabrizord.gzgestao.model.Company;
 import br.com.gabrizord.gzgestao.service.CompanyService;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CompanyController {
@@ -18,9 +18,22 @@ public class CompanyController {
     }
 
     @GetMapping("/companies")
-    public String viewCompanies(Model model) {
-        List<Company> companies = companyService.getAllCompanies();
-        model.addAttribute("companies", companies);
+    public String viewCompanies(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "12") int size,
+            @RequestParam(defaultValue = "id") String sortField,
+            @RequestParam(defaultValue = "asc") String sortDirection,
+            Model model) {
+
+        Page<Company> companyPage = companyService.getPaginatedCompanies(page, size, sortField, sortDirection);
+        model.addAttribute("companyPage", companyPage);
+
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", companyPage.getTotalPages());
+        model.addAttribute("totalItems", companyPage.getTotalElements());
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDirection", sortDirection);
+        model.addAttribute("reverseSortDirection", sortDirection.equals("asc") ? "desc" : "asc");
         return "company/company";
     }
 }
